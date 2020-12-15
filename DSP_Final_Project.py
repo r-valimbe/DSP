cbb6645f5bd28f1630efaec80e2d4e265283be1b
r-valimbe <58121098+r-valimbe@users.noreply.github.com>
2020-12-15 11:35:53 -0500

    Update DSP_Final_Project.py
    Added the manual radiobutton and placed frequency and amplitude scales for manual mode.

--- DSP_Final_Project.py
+++ DSP_Final_Project.py
@@ -3,6 +3,7 @@ import tkinter as tk
 import wave
 import time
 root = tk.Tk()
+
 def start(scale, entry, label, v):
     '''
     In here we will first take the number of seconds they want to record from the scale and set that to the Duration
@@ -14,6 +15,7 @@ def start(scale, entry, label, v):
     Then take the file name from entry bxo and save it as a wav file with that file name
 
     '''
+
 
     if len(entry.get()) == 0: #can try and get rid of invalid characters when saving file too but that won't be necessary
         label['text'] = 'File name cannot be empty'
@@ -35,7 +37,8 @@ def start(scale, entry, label, v):
         elif v.get()  == 4:
         #voice4(output_wavfile, duration)
             print("4")
-
+        else:
+            print("5")
         #after whatever operation we do
         label['text'] = 'Successfully saved ' + output_wavfile + '.wav file'
 
@@ -93,20 +96,32 @@ record_length_scale.place(x=0, y = 120)
 
 radio1 = tk.Radiobutton(root, text="First Voice", variable = v, value = 1)
 radio1.place(x=0, y=200)
-radio2= tk.Radiobutton(root, text="Second Voice", variable = v, value = 2)
+radio2 = tk.Radiobutton(root, text="Second Voice", variable = v, value = 2)
 radio2.place(x=0, y=220)
-radio3= tk.Radiobutton(root, text="Third Voice", variable = v, value = 3)
+radio3 = tk.Radiobutton(root, text="Third Voice", variable = v, value = 3)
 radio3.place(x=0, y=240)
-radio4= tk.Radiobutton(root, text="Fourth Voice", variable = v, value = 4)
+radio4 = tk.Radiobutton(root, text="Fourth Voice", variable = v, value = 4)
 radio4.place(x=0, y=260)
+radio5 = tk.Radiobutton(root, text="Manual", variable = v, value = 5)
+radio5.place(x=0, y=280)
+
+frequency_label = tk.Label(root, text="Frequency")
+frequency_label.place(x= 200, y= 180)
+frequency = tk.Scale(root, from_ = 0, to_ = 600, orient = tk.VERTICAL)
+frequency.place(x=200, y = 200)
+
+amplitude_label = tk.Label(root, text="Amplitude")
+amplitude_label.place(x= 270, y= 180)
+amplitude = tk.Scale(root, from_ = 0, to_ = 100, orient = tk.VERTICAL)
+amplitude.place(x=270, y = 200)
 
 file_name_label = tk.Label(root, text="What file name would you like to save this wav file as?")
-file_name_label.place(x=0, y =300)
+file_name_label.place(x=0, y =320)
 file_name_entry = tk.Entry(root)
-file_name_entry.place(x=300, y=300)
+file_name_entry.place(x=300, y=320)
 
 start_button = tk.Button(root, text="Start Recording", command = lambda:start(record_length_scale, file_name_entry,indicator_label, v))
-start_button.place(x=0, y = 330)
+start_button.place(x=205, y = 340)
 
 indicator_label = tk.Label(root, text = "")
 indicator_label.place(x=0, y = 360)
